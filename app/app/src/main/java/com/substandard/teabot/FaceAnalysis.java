c61cad28b20fc71baef75d9a20553a4cef538003
Pav Sidhu <pav@pavsidhu.com>
2018-11-25 03:48:06 +0000

    Add working camera, yayayay

---
+++ app/app/src/main/java/com/substandard/teabot/FaceAnalysis.java
@@ -0,0 +1,58 @@
+package com.substandard.teabot;
+
+import android.os.AsyncTask;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+class FaceAnalysis {
+    private String key = "{147ea959552a46d1b8751422c7afa51b}";
+
+    static void analyse(byte[] picture) {
+        new FaceDetectApi().execute(picture);
+    }
+
+    static void evaulateEmotion(double smile) {
+//        if (smile > 0.5) {
+//            new TeaBotApi().execute(60);
+//        }
+    }
+
+    private static class FaceDetectApi extends AsyncTask<byte[], HttpResponse<JsonNode>, HttpResponse<JsonNode>> {
+        protected HttpResponse<JsonNode> doInBackground(byte[]... picture) {
+            try {
+                return Unirest.post("https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect")
+                        .header("Ocp-Apim-Subscription-Key", "7610f1fd8d744d16a1f46cf4c2ef35d1")
+                        .header("Content-Type", "application/octet-stream")
+                        .queryString("returnFaceId", "false")
+                        .queryString("returnFaceLandmarks", "false")
+                        .queryString("returnFaceAttributes", "smile")
+                        .body(picture[0])
+                        .asJson();
+            } catch (UnirestException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(HttpResponse<JsonNode> response)  {
+            try {
+                if (response.getStatus() == 200) {
+                    JSONArray body = response.getBody().getArray();
+
+                    double smile = body.getJSONObject(0).getJSONObject("faceAttributes").getDouble("smile");
+                    evaulateEmotion(smile);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
